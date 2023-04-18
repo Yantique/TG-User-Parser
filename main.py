8633652cde4be2d0fce5d1031fc826d9bfec6342
Yan <yan.leontev21@gmail.com>
2023-04-18 06:28:32 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from pyrogram import Client
+
+
+API_ID = '<API_ID>'  # Заменить на свой API_ID
+API_HASH = '<API_HASH>'  # Заменить на свой API_ID
+
+
+def get_chat_ids():
+    with client:
+        dialogs = client.get_dialogs()
+
+        print("Список чатов:")
+        for dialog in dialogs:
+            print(f"Название: {dialog.chat.title}; id: {dialog.chat.id}")
+
+
+def get_members(chat_id):
+    with client:
+        members = client.get_chat_members(chat_id)
+        with open('members.txt', 'w', encoding='utf-8') as f:
+            f.write("user id, name\n")
+            for member in members:
+                f.write(f"{member.user.id}, {member.user.first_name}\n")
+
+
+if __name__ == '__main__':
+    try:
+        client = Client('account', api_id=API_ID, api_hash=API_HASH)
+        get_chat_ids()
+        chat = int(input('Введите id чата: '))
+        get_members(chat)
+    except:
+        print('Что-то пошло не так :(')
